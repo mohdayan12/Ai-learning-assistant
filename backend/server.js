c1c09a11ad5b5bebab8d9670a68327ab96f8bfa2
mohd ayan <mohdayan7@gmail.com>
2026-01-26 11:57:16 +0530

    intial server setup with express

---
+++ backend/server.js
@@ -0,0 +1,50 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import connectDB from './config/db.js';
+import errorHandler from './middleware/errorHandler.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app =express();
+
+connectDB();
+
+app.use(cors({
+    origin:'*',
+    methods:['GET','POST','PUT','DELETE'],
+    alowedHeaders:['Content-Type','Authorization'],
+    credentials:true 
+}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// static folder for uploads
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+// Routes
+
+ app.use(errorHandler);
+
+ //404 handler
+ app.use((req, res) => {
+    res.status(404).json({
+     success:false,
+     error: 'Route not found',
+     statusCode:404 });
+ });
+
+
+ const PORT = process.env.PORT || 5000;
+ app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+ });
+
+ process.on('unhandledRejection',(err)=>{
+    console.error(`Error: ${err.message}`);
+    process.exit(1);
+ });
