5773af30220d66519982e92900402fcb949f814a
mohd ayan <mohdayan7@gmail.com>
2026-01-27 12:12:08 +0530

    add User model with schema and password hashing functionality

---
+++ backend/models/User.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please provide a username"],
+      unique: true,
+      trim: true,
+      minLength: [3, "Username must have at least 3 character"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, "Please provide a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minLength: [6, "Password must be at least 6 character"],
+      select: false,
+    },
+    profileImage: {
+      type: String,
+      default: null,
+    },
+  },
+  { timestamps: true },
+);
+
+// pre-save hook to hash password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
