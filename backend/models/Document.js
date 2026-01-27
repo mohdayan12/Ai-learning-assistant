4d4956b67ce97fc32729e8e3202551c758ed9ea7
mohd ayan <mohdayan7@gmail.com>
2026-01-27 13:52:00 +0530

    add Document model with schema for user documents, including metadata and content structure

---
+++ backend/models/Document.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const documentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: [true, "Please provide the document title"],
+      trim: true,
+    },
+    fileName: {
+      type: String,
+      requird: true,
+    },
+    filePath: {
+      type: String,
+      required: true,
+    },
+    fileSize: {
+      type: Number,
+      required: true,
+    },
+    extractedText: {
+      type: String,
+      required: true,
+    },
+    chunks: [
+      {
+        content: {
+          type: String,
+          required: true,
+        },
+        pageNumber: {
+          type: Number,
+          default: 0,
+        },
+        chunkIndex: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    uploadDate: {
+      type: Date,
+      default: Date.now,
+    },
+    lastAccessed: {
+      type: Date,
+      default: Date.now,
+    },
+    status: {
+      type: String,
+      enum: ["processing", "ready", "failed"],
+      default: "processing",
+    },
+  },
+  { timestammps: true },
+);
+documentSchema.index({ userId: 1, uploadDate: -1 });
+const Document = mongoose.model("Document", documentSchema);
+export default Document;
