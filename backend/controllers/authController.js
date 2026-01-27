9478ca5ae30256bc0ab854438b74c18a389ea78d
mohd ayan <mohdayan7@gmail.com>
2026-01-27 19:02:09 +0530

    add user login functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,76 +1,107 @@
-import jwt from 'jsonwebtoken'
-import User from '../models/User.js'
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-const generateToken=(id)=>{
- return jwt.sign({id},process.env.JWT_SECRET,{
-  expiresIn:process.env.JWT_EXPIRE || "7d"
- });
-}
-
-export const register=async(req,res,next)=>{
- try {
- const {username,email,password}=req.body;
- const userExists=await User.findOne({$or:[{email}]});
- if(userExists){
-  return res.status(400).json({
-  success:false,
-  error:userExists.email===email?'Email already registered':"Username is already taken",
-  statusCode:400
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE || "7d",
   });
- };
- const user= await User.create({
- username,email,password
- });
- const token=generateToken(user._id)
- res.status(201).json({
-  success:true,
-  data:{
-    user:{
-     id:user._id,
-     username:user.username,
-     email:user.email,
-     profileImage:user.profileImage,
-     createdAt:user.createdAt,
-    },
-    token,
-  },
-  message:'User register successfully'
- })
-    
- } catch (error) {
-  next(error)
-    
- }
-}
+};
 
-export const login =async(res,req,next)=>{
+export const register = async (req, res, next) => {
   try {
-    
+    const { username, email, password } = req.body;
+    const userExists = await User.findOne({ $or: [{ email }] });
+    if (userExists) {
+      return res.status(400).json({
+        success: false,
+        error:
+          userExists.email === email
+            ? "Email already registered"
+            : "Username is already taken",
+        statusCode: 400,
+      });
+    }
+    const user = await User.create({
+      username,
+      email,
+      password,
+    });
+    const token = generateToken(user._id);
+    res.status(201).json({
+      success: true,
+      data: {
+        user: {
+          id: user._id,
+          username: user.username,
+          email: user.email,
+          profileImage: user.profileImage,
+          createdAt: user.createdAt,
+        },
+        token,
+      },
+      message: "User register successfully",
+    });
   } catch (error) {
-    
+    next(error);
   }
-}
+};
 
-export const getProfile=async(res,req,next)=>{
- try {
-    
- } catch (error) {
-    
- }
-}
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        error: "Please provide email and password",
+        statusCode: 400,
+      });
+    }
 
-export const updateProfile=async(res,req,next)=>{
-try {
-    
-} catch (error) {
-    
-}
-}
+    const user = await User.findOne({ email }).select("+password");
 
-export const changePassword=async(res,req,next)=>{
-try {
-    
-} catch (error) {
-    
-}
-}
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        error: "Invalid credientials",
+        statusCode: 401,
+      });
+    }
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        error: "Invalid credientials",
+        statusCode: 401,
+      });
+    }
+    const token = generateToken(user._id);
+    res.status(200).json({
+      success: true,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+      token,
+      message: "User logged in successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getProfile = async (res, req, next) => {
+  try {
+  } catch (error) {}
+};
+
+export const updateProfile = async (res, req, next) => {
+  try {
+  } catch (error) {}
+};
+
+export const changePassword = async (res, req, next) => {
+  try {
+  } catch (error) {}
+};
