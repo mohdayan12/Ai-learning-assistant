7f32c0e2aa0928245c347e76bce0208bf8dcb4b1
mohd ayan <mohdayan7@gmail.com>
2026-02-03 11:03:33 +0530

    add flashcard routes to server configuration

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import connectDB from './config/db.js';
 import errorHandler from './middleware/errorHandle.js';
 import authRoutes from './routes/authRoutes.js'
 import documentRoutes from './routes/documentRoutes.js'
+import flashcardRoutes from './routes/flashcardRoutes.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -31,6 +32,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 // Routes
 app.use('/api/auth',authRoutes)
 app.use('/api/documents',documentRoutes)
+app.use('/api/flashcards',flashcardRoutes)
 
 
  app.use(errorHandler);
