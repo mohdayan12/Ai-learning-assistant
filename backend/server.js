f44b00b8b6909f0880e8e3329a869a993479b83a
mohd ayan <mohdayan7@gmail.com>
2026-02-03 22:08:16 +0530

    add AI routes to server configuration

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import errorHandler from './middleware/errorHandle.js';
 import authRoutes from './routes/authRoutes.js'
 import documentRoutes from './routes/documentRoutes.js'
 import flashcardRoutes from './routes/flashcardRoutes.js'
+import aiRoutes from './routes/aiRoutes.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -33,6 +34,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use('/api/auth',authRoutes)
 app.use('/api/documents',documentRoutes)
 app.use('/api/flashcards',flashcardRoutes)
+app.use('/api/aiRoutes',aiRoutes)
 
 
  app.use(errorHandler);
